e061c3136116c16976d1835a3debc484de294c33
João Nogueira <147201184+joaoNogueiradev@users.noreply.github.com>
2025-05-30 03:13:38 +0000

    update: visual style and characters image inclusion

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import ModalStory from "./components/ModalStory";
 import { useFetch } from "./hooks/useFetch";
+import loadingImg from "./assets/loadingImg.gif"
 
 const url = "https://6835be8fcd78db2058c2f60b.mockapi.io/characters";
 
@@ -15,22 +16,38 @@ function App() {
   return (
     <>
       <div className="h-screen flex items-center text-center justify-center">
-        {loading && <p>Carregando dados...</p>}
+        {loading && <img src={loadingImg}/>}
         {!loading && (
           <ul>
+            <h1 className="font-bold m-15 text-3xl ">CHARACTERS LIST</h1>
             {chars &&
-              chars.map((character) => {
+              chars.map((character, index) => {
                 return (
                   <li
-                    key={character.id}
-                    onClick={() => {
-                      setOpenModal(true);
-                      setSelectedCharacter(character);
+                  key={character.id}
+                  onClick={() => {
+                    setOpenModal(true);
+                    setSelectedCharacter(character);
+                  }}
+                  className="text-5xl sm:text-6xl text-gray-700 font-bold cursor-pointer w-screen h-[200px] flex items-center justify-between px-12 border-y-2 border-gray-200 hover:bg-gray-100 transition-all duration-300"
+                style={{
+                backgroundImage: `url(${character.image})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: index % 2 === 0 ? 'left 120px top' : 'right 120px top',
+                backgroundSize: 'auto 160%',
+              }}
+                >
+                  {/* Espaço transparente para texto */}
+                  <span
+                    className="bg-white/80 px-6 py-3 rounded-xl shadow-md backdrop-blur-sm"
+                    style={{
+                      marginLeft: index % 2 === 0 ? 'auto' : '0',
+                      marginRight: index % 2 !== 0 ? 'auto' : '0',
                     }}
-                    className="text-xl cursor-pointer"
                   >
                     {character.name.toUpperCase()}
-                  </li>
+                  </span>
+                </li>
                 );
               })}
             {selectedCharacter && (
@@ -42,7 +59,7 @@ function App() {
                 }}
               >
                 <h2 className='font-bold '>
-                  from: {selectedCharacter.media}
+                  FROM: {selectedCharacter.media}
                 </h2>
                 {selectedCharacter.story}
               </ModalStory>
